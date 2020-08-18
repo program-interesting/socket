254ea02a9c153cecc7c9967e09bef06e4ac2628e
lidi3 <lidi3@100tal.com>
2020-08-18 16:05:41 +0800

    help

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# hello socket
+# hello  socket  For  PHP
+
+## 1、修改 web.html 文件中的host
+```html
+    //TODO 将 127.0.0.1 替换为服务的真实 Host, 即可
+    let host = '127.0.0.1';
+```
+
+## 2、修改 index.php 文件中的host
+```php
+    //TODO 将 127.0.0.1 替换为服务的真实 Host, 即可
+    $host     = '127.0.0.1';
+```
+
+## 3、启动服务：访问 host + 8088 即可
+```php
+   1、启动服务端：php SocketService.php
+   
+   2、启动客户端： php -S 0.0.0.0:8088 -t ./ &
+   
+   3、访问host+8088：
+   
+   🤖️️ 点击开始聊天 👉: web.html
+   
+   点击后，即可开始聊天，balabala ...
+```
+
--- index.php
+++ index.php
@@ -1,10 +1,42 @@
 <?php
+//TODO 将 127.0.0.1 替换为服务的真实 Host, 即可
+$host     = '127.0.0.1';
 /**
- * 获取资源
+ * 动态的路径名称
  */
-$html = file_get_contents("web.html");
+$realPath = '';
+/**
+ * 即将收集的数据
+ */
+$allData  = array();
+function getDir($path, $realPath, $host, &$allData)
+{
+    if (is_dir($path)) {
+        $dir = scandir($path);
+        foreach ($dir as $value) {
+            $sub_path = $path . '/' . $value;
+            if ($value == '.' || $value == '..') {
+                continue;
+            } elseif (is_dir($sub_path)) {
+                $realPath = $value;
+                getDir($sub_path, $realPath, $host, $allData);
+            } else {
+                if ($value == 'web.html') {
+                    $url  = 'http://'.$host.':8088/' . $path . '/' . $value;
+                    $allData[$realPath][] = [$value, $url];
+                    echo "<a target='_blank' href='$url'>🤖️️点击开始聊天👉: " . $value . '</a><br/>';
+                }
+            }
+        }
+    }
+}
 
 /**
- * 聊天会话
+ * 当前路径名称
  */
-echo $html;
+$path = './';
+
+/**
+ * 立刻调用
+ */
+getDir($path, $realPath, $host, $allData);
